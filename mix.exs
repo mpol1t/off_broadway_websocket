37afd0eab5e5b57ab96fdbb805d46498b33daab7
Michal Polit <mpolit@protonmail.com>
2024-11-23 23:04:35 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OffBroadwayWebsocket.MixProject do
   def project do
     [
       app: :off_broadway_websocket,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
