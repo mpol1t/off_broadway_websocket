1c671930789304c2fe74131bfc604490c0deb14d
Michal Polit <mpolit@protonmail.com>
2025-05-25 17:45:27 +0100

    Add retry logic

--- lib/off_broadway_websocket/client_behaviour.ex
+++ lib/off_broadway_websocket/client_behaviour.ex
@@ -4,9 +4,8 @@ defmodule OffBroadwayWebSocket.ClientBehaviour do
   @callback connect(
               url :: String.t(),
               path :: String.t(),
-              opts :: {any(), any()},
+              gun_opts :: map(),
               await_timeout :: non_neg_integer(),
-              connect_timeout :: non_neg_integer(),
               headers :: list()
             ) :: {:ok, map()} | {:error, any()}
 end
